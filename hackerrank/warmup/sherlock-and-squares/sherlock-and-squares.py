99c6852cfbe8eebdbf14ce379cbcac5c3511a70b
clovisleoncio <clovisleoncio@gmail.com>
2014-11-05 09:37:56 -0200

    Correct answer for Sherlock and Squares

--- hackerrank/warmup/sherlock-and-squares/sherlock-and-squares.py
+++ hackerrank/warmup/sherlock-and-squares/sherlock-and-squares.py
@@ -7,7 +7,12 @@ numberOfTestCases = int(raw_input());
 for i in xrange(numberOfTestCases):
     a, b = [int(x) for x in raw_input().split()];
 
-    a = int(sqrt(a));
-    b = int(sqrt(b));
+    sqrA = sqrt(a);
+    sqrB = sqrt(b);
 
-    print b - a
+    a = int(sqrA);
+    if sqrA % 1 > 0:
+    	a = a + 1;
+    b = int(sqrB);
+
+    print b - a + 1
