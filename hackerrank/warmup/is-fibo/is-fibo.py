2fd36b59d717a6cadb2f90e78b78e1d051796023
clovisleoncio <clovisleoncio@gmail.com>
2014-11-11 19:26:50 -0200

    Adding Is Fibo python solution

---
+++ hackerrank/warmup/is-fibo/is-fibo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from math import sqrt;
+
+numberOfTestCases = int(raw_input());
+
+for i in xrange(numberOfTestCases):
+    n = long(raw_input());
+
+    if sqrt(5 * (n**2) + 4) % 1 == 0 or sqrt(5 * (n**2) - 4) % 1 == 0:
+        print 'isFibo';
+    else:
+        print 'isNotFibo';
