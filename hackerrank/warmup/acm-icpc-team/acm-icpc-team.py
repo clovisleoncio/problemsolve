def97bddb9385c8ec5fef6c470b11b883cc1c27d
clovisleoncio <clovisleoncio@gmail.com>
2014-11-12 11:16:19 -0200

    Adjusting ACM-ICPC

--- hackerrank/warmup/acm-icpc-team/acm-icpc-team.py
+++ hackerrank/warmup/acm-icpc-team/acm-icpc-team.py
@@ -2,19 +2,20 @@
 
 inputSize = int(raw_input().split()[0]);
 
-members = [int(raw_input(), 2) for i in xrange(inputSize)];
+members = [long(raw_input(), 2) for i in xrange(inputSize)];
 
 maxSkills = 0;
 numberOfTeams = 0;
 
 for i in xrange(len(members)):
     for j in xrange(i + 1, len(members)):
-        numberOfSkills = members[i] | members[j]
+        skills = members[i] | members[j]
+        numberOfSkills = bin(skills).count('1');
         if numberOfSkills == maxSkills:
             numberOfTeams = numberOfTeams + 1;
         elif numberOfSkills > maxSkills:
             maxSkills = numberOfSkills;
             numberOfTeams = 1;
 
-print bin(maxSkills).count('1');
+print maxSkills;
 print numberOfTeams;
