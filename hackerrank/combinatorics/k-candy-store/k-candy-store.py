a6297613554797505aab8f142f2a2eff6da8ed7c
clovisleoncio <clovisleoncio@gmail.com>
2014-11-18 20:56:03 -0200

    Adding K Candy Store python solution

---
+++ hackerrank/combinatorics/k-candy-store/k-candy-store.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+fatoriais = [1, 1];
+
+def fatorial(numero):
+
+	if numero > len(fatoriais) - 1:
+		for x in xrange(len(fatoriais), numero + 1):
+			fatoriais.append(fatoriais[x - 1] * x);
+	return fatoriais[numero];
+
+for x in xrange(int(raw_input())):
+	n = int(raw_input());
+	k = int(raw_input());
+
+	print fatorial(n + k - 1) / (fatorial(k) * fatorial(n - 1));
