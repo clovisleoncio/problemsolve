6afad6e06ba230991a8a2489dad2dc031753b9b5
clovisleoncio <clovisleoncio@gmail.com>
2014-11-18 23:36:53 -0200

    Optimizing algorithm

--- hackerrank/warmup/manasa-and-stones/manasa-and-stones.py
+++ hackerrank/warmup/manasa-and-stones/manasa-and-stones.py
@@ -1,24 +1,17 @@
 #!/usr/bin/env python
 
-def calc(a, b, n, value, list):
-	if n == 1:
-		list.add(value + a);
-		list.add(value + b);
-	else:
-		calc(a, b, n - 1, value + a, list);
-		calc(a, b, n - 1, value + b, list);
-
-numberOfTestCases = int(raw_input());
-
-for i in xrange(numberOfTestCases):
+for i in xrange(int(raw_input())):
     n = int(raw_input()) - 1;
+
     a = int(raw_input());
     b = int(raw_input());
 
-    results = set([]);
+    value = n * a;
 
-    calc(a, b, n, 0, results);
+    results = set([value]);
 
-    results = sorted(results);
+    for x in xrange(n):
+        value = value - a + b;
+        results.add(value);
 
-    print ' '.join(str(x) for x in results);
+    print ' '.join(str(x) for x in sorted(results));
